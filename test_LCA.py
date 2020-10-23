778273f9d08c9050883abc8d9e84ebe1e1c3404d
byrnec68 <byrnec68@tcd.ie>
2020-10-23 17:16:27 +0100

    Enhanced test_LCA.py

--- test_LCA.py
+++ test_LCA.py
@@ -9,6 +9,9 @@ class TestLCA(unittest.TestCase):
         self.assertEqual(LCA.findLCA(root, 4, 5), 2) #Expected answer == 2
         self.assertEqual(LCA.findLCA(root, 3,2),1)#Expected answer == 1
         self.assertEqual(LCA.findLCA(empty, 2, 3), -1)#Null list, expected answer -1
+        self.assertEqual(LCA.findLCA(root, 1,8),-1)#Expected answer == 1, only left node exists
+        self.assertEqual(LCA.findLCA(root, -8,1),-1)#Expected answer == 1, only right node exists
+        
 
 if __name__ == '__main__':
     unittest.main()
